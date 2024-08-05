915bd913dc09369c0a4e59ad8830abf2349ec2f4
atoposyz <2335483715@qq.com>
2024-08-05 11:35:34 +0800

    first commit

---
+++ src/components/DropZone.vue
@@ -0,0 +1,44 @@
+<!-- src/components/DropZone.vue -->
+<template>
+    <div
+      class="drop-zone"
+      @dragover.prevent
+      @drop="onDrop"
+    >
+      <div v-if="droppedImage" class="dropped-image" :style="{ backgroundImage: `url(${droppedImage})` }"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        droppedImage: null,
+      };
+    },
+    methods: {
+      onDrop(event) {
+        const imageSrc = event.dataTransfer.getData('imageSrc');
+        this.droppedImage = imageSrc;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .drop-zone {
+    width: 200px;
+    height: 200px;
+    border: 2px dashed #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .dropped-image {
+    width: 100px;
+    height: 100px;
+    background-size: cover;
+    background-position: center;
+  }
+  </style>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<!-- src/views/Home.vue -->
+<template>
+  <div class="home">
+    <DraggableImage imageSrc="/1.jpg" />
+    <DraggableImage imageSrc="/2.jpg" />
+    <DropZone />
+  </div>
+</template>
+
+<script>
+import DraggableImage from '@/components/DraggableImage.vue';
+import DropZone from '@/components/DropZone.vue';
+
+export default {
+  components: {
+    DraggableImage,
+    DropZone,
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  gap: 20px;
+}
+</style>
