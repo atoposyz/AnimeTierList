2eb75d8562aea5feb0cac9d04cd16f08221d6d0b
atoposyz <2335483715@qq.com>
2024-08-06 17:37:48 +0800

    删除了没用的路由

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,54 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
-      <nav style="display: none">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="home">
+    <SearchAnimeBox v-if="ifsearch" @closesearchbox="handleclosesearchbox" />
+    <template v-for="rankitem in ranklist" :key="rankitem.name">
+      <ImageRankZone :rankname=rankitem.name :color=rankitem.color />
+    </template>
+    <SortableImageList @opensearchbox="handleopensearchbox" />
+  </div>
 </template>
 
+<script>
+
+import SortableImageList from '@/components/SortableImageList.vue';
+import ImageRankZone from '@/components/ImageRankZone.vue';
+import SearchAnimeBox from '@/components/SearchAnimeBox.vue';
+
+export default {
+  components: {
+    ImageRankZone,
+    SortableImageList,
+    SearchAnimeBox,
+  },
+  data() {
+    return {
+      ranklist: [
+        { name: "老婆！！", color: "OrangeRed " },
+        { name: "老婆！", color: "Orange" },
+        { name: "老婆？", color: "GoldenRod" },
+        { name: "朋友吧", color: "Gold" },
+        { name: "一般", color: "Gray" },],
+      ifsearch: false,
+    }
+  },
+  methods: {
+    handleopensearchbox() {
+      this.ifsearch = true
+    },
+    handleclosesearchbox() {
+      this.ifsearch = false
+    }
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
+.home {
   display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  gap: 20px;
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import emitter from './utils/eventBus'
 
 const app = createApp(App)
-
-app.use(router)
 
 app.config.globalProperties.$bus = emitter
 
