a64ec2e4b94b117528f8c52e57b9e2974c297a13
atoposyz <2335483715@qq.com>
2024-11-15 11:29:49 +0800

    fix: :art: 导出为图片时删除settings

--- src/App.vue
+++ src/App.vue
@@ -144,12 +144,16 @@ export default {
 		},
 		captureimg() {
 			const element = this.$refs.imageRankTable;
+			// 克隆整个元素，保留原始网页不受影响
+			const settingsDivs = element.querySelectorAll('div.settings'); // 查找所有 class 为 settings 的 div
+			settingsDivs.forEach(div => div.style.display = "none"); // 删除每一个找到的 div
 			html2canvas(element, { useCORS: true }).then(canvas => {
 				const link = document.createElement('a');
 				link.href = canvas.toDataURL('image/png');
 				link.download = 'ImageRankTable.png';
 				link.click();
 			});
+			settingsDivs.forEach(div => div.style.display = "flex");
 			this.changesave();
 		},
 		savejson() {
